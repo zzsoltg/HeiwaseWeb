ca297fb8cf05d7f97cb0b27cc353d7cb449b7b29
zimmermannzsg <zimmermannzsg@griffsoft.hu>
2026-08-18 14:14:06 +0200

    Added language selector component with layout code refactor

---
+++ Heiwase.App.Blazor/Components/Layout/NavigationMenu/NavigationMenu.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using System.Globalization;
+
+namespace Heiwase.App.Blazor.Components.Layout.NavigationMenu;
+
+public partial class NavigationMenu
+{
+
+    private bool _isMenuOpen = false;
+
+    private void ToggleMenu()
+        => _isMenuOpen = !_isMenuOpen;
+
+    private void CloseMenu()
+        => _isMenuOpen = false;
+}
