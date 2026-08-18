fae5645d7704b5bc32f333697d7e05ee2a81b7f5
zimmermannzsg <zimmermannzsg@griffsoft.hu>
2026-08-18 11:19:43 +0200

    Fixed hall of fame dialogs

--- Heiwase.App.Blazor/Components/Pages/HallOfFameSection/HallOfFameSection.Member.cs
+++ Heiwase.App.Blazor/Components/Pages/HallOfFameSection/HallOfFameSection.Member.cs
@@ -6,7 +6,7 @@ public partial class HallOfFameSection
     {
         public string Name { get; set; } = "";
         public string Title { get; set; } = "";
-        public string Description { get; set; } = "";
+        public List<string> Description { get; set; } = [];
         public string ImagePath { get; set; } = "";
     }
 }
