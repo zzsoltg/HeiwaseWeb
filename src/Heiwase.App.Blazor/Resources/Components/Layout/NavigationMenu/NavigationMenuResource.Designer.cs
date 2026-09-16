906e691aa4d0f473128c1bb629ae76b71d122092
zzsoltg <zimmermannzsoltgabor@gmail.com>
2026-09-16 14:39:40 +0200

    Timtable section replacement and layout initialization

--- src/Heiwase.App.Blazor/Resources/Components/Layout/NavigationMenu/NavigationMenuResource.Designer.cs
+++ src/Heiwase.App.Blazor/Resources/Components/Layout/NavigationMenu/NavigationMenuResource.Designer.cs
@@ -116,15 +116,6 @@ namespace Heiwase.App.Blazor.Components.Layout.NavigationMenu {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Edzésidőpontok.
-        /// </summary>
-        public static string Timetable {
-            get {
-                return ResourceManager.GetString("Timetable", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Edzéstípusok.
         /// </summary>
         public static string WhyUs {
